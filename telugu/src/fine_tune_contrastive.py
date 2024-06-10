c12e6fd71e19be0a9388ff38462f833ad0966ed9
MysticShadow427 <devendrakayande427@gmail.com>
2024-06-10 22:03:40 +0530

    added code for telugu

---
+++ telugu/src/fine_tune_contrastive.py
@@ -0,0 +1,179 @@
+from collections import defaultdict
+import argparse
+from marathi.src.model_trainer import train_epoch,eval_model
+from marathi.src.dataloaders import create_data_loader
+from marathi.src.load_llm import SpanClassifier,LorafiedSpanClassifier
+from marathi.src.custom_losses import SupConWithCrossEntropy,SupervisedContrastiveLoss
+from marathi.src.utils import plot_accuracy_loss,save_training_history,create_directory
+from transformers import AutoModel,AutoTokenizer,get_linear_schedule_with_warmup,AutoModelForSequenceClassification
+from peft import LoraConfig, get_peft_model
+import torch
+import torch.nn as nn
+import pandas as pd
+import numpy as np
+from marathi.src.evaluate_model import get_confusion_matrix,get_predictions,get_scores,get_classification_report
+from marathi.src.augment_data import random_undersample
+from marathi.src.load_llm import SpanClassifier
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, help="number of epochs for training")
+    parser.add_argument("--learning_rate",type=float,help="learning rate")
+    parser.add_argument("--batch_size",type=int,help="batch size forr training")
+    parser.add_argument("--num_views",type=int,help="number of views for the contrastive loss")
+    parser.add_argument("--lora",type=int,help="add adapter layers or not")
+
+    args = parser.parse_args()
+
+    EPOCHS = args.epochs
+    batch_size = args.batch_size
+    learning_rate = args.learning_rate
+    num_views = args.num_views
+    lora = args.lora
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print('\033[96m' + 'Device : ',device + '\033[0m')
+    print()
+
+    train_df = pd.read_csv('/content/lt-edi-2024/telugu/data/homo_Telugu_train.csv')
+    val_df = pd.read_csv('/content/lt-edi-2024/telugu/data/homo_Telugu_dev.csv')
+    test_df = pd.read_csv('/content/lt-edi-2024/telugu/data/homo_Telugu_test.csv')
+    # train_df = pd.concat([train_df, val_df])
+    train_df.rename(columns={'Text': 'content','Category ':'label'}, inplace=True)
+    val_df.rename(columns={'Text': 'content','Category ':'label'}, inplace=True)
+    test_df.rename(columns={'Text': 'content','Category ':'label'}, inplace=True)
+    print('\033[96m' + 'Loaded Training, validation and test dataframes'+ '\033[0m')
+    print()
+    # train_df = random_undersample(train_df)
+
+    print('\033[96m' + 'Preprocessing of Data done'+ '\033[0m')
+    print()
+    directory_path = "/content/lt-edi-2024/telugu/artifacts"
+    create_directory(directory_path)
+    # Label to index
+    tags = train_df.label.unique().tolist()
+    num_classes = len(tags)
+    class_to_index = {tag: i for i, tag in enumerate(tags)}
+    # Encode labels
+    train_df["label"] = train_df["label"].map(class_to_index)
+    val_df["label"] = val_df["label"].map(class_to_index)
+    test_df["label"] = test_df["label"].map(class_to_index)
+    class_names = ['None of the categories','Homophobia','Transphobia']
+    num_labels = 3
+
+    id2label = {i: label for i, label in enumerate(class_names)}
+    label2id = {label: i for i,label in enumerate(class_names)}
+    checkpoint = 'l3cube-pune/telugu-bert'
+
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    print('\033[96m' + 'Tokenizer Loaded'+ '\033[0m')
+    print()
+    model = None
+    if not lora:
+        model = SpanClassifier().to(device)
+    else:
+        model =  LorafiedSpanClassifier().to(device)
+        
+    print('\033[96m' + 'Model Telugu BERT Loaded'+ '\033[0m')
+    print()
+
+    train_data_loader = create_data_loader(train_df,tokenizer=tokenizer,max_len=35,batch_size=batch_size)
+    val_data_loader = create_data_loader(val_df,tokenizer=tokenizer,max_len=35,batch_size=batch_size)
+    test_data_loader = create_data_loader(test_df,tokenizer=tokenizer,max_len=35,batch_size=batch_size)
+    print('\033[96m' + 'Dataloaders created')
+    print()
+
+    total_steps = len(train_data_loader) * EPOCHS
+
+    loss_fn = SupConWithCrossEntropy()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+    scheduler = get_linear_schedule_with_warmup(
+                optimizer,
+                num_warmup_steps=0,
+                num_training_steps=total_steps
+                )
+    print('\033[96m' + 'Loss function,Optimizer and Learning Rate Schedule set'+ '\033[0m')
+    print()
+
+    history = defaultdict(list)
+    best_acc = 0
+    print('\033[96m' + 'Starting training...'+ '\033[0m')
+    print()
+
+    for epoch in range(EPOCHS):
+        print('\033[96m' + f'Epoch {epoch + 1}/{EPOCHS}'+ '\033[0m')
+        print('\033[96m' + '-' * 10+ '\033[0m')
+
+        train_acc, train_loss = train_epoch(
+            model,
+            train_data_loader,
+            loss_fn,
+            optimizer,
+            device,
+            scheduler,
+            len(train_df),
+            num_views,
+            epoch
+        )
+
+        print('\033[96m' + f'Train loss {train_loss} accuracy {train_acc}'+ '\033[0m')
+
+        val_acc, val_loss = eval_model(
+            model,
+            test_data_loader,
+            loss_fn,
+            device,
+            len(test_df),
+            num_views
+        )
+
+        print('\033[96m' + f'Val   loss {val_loss} accuracy {val_acc}'+ '\033[0m')
+        print()
+
+        history['train_acc'].append(train_acc)
+        history['train_loss'].append(train_loss)
+        history['val_acc'].append(val_acc)
+        history['val_loss'].append(val_loss)
+
+        if val_acc > best_acc:
+            torch.save(model.state_dict(), '/content/lt-edi-2024/telugu/artifacts/best_model_state_full_fine_tune.bin')
+            torch.save(obj=model.state_dict(),f='/content/lt-edi-2024/telugu/artifacts/best_model_full_fine_tune.pth')
+            # model.save_pretrained('/content/lt-edi-2024/marathi/artifacts/marbert',from_pt = True)
+            best_acc = val_acc
+    print()
+    print('\033[96m' + 'Training finished'+ '\033[0m')
+    print()
+    # plot_accuracy_loss(history)
+    # history_csv_file_path = "/content/lt-edi-202/marathi/artifacts/history.csv"
+    # save_training_history(history=history,path=history_csv_file_path)
+    # model = SpanClassifier()
+    # model.load_state_dict(torch.load(f='/content/lt-edi-2024/marathi/artifacts/best_model_full_fine_tune.pth'))
+    # model = model.to('cuda')
+    print('\033[96m' + 'Training History saved'+ '\033[0m')
+    print()
+
+    print('\033[96m' + 'Getting Predictions...'+ '\033[0m')
+    print()
+    y_review_texts_test, y_pred_test, y_pred_probs_test, y_test = get_predictions(model,test_data_loader)
+    y_review_texts_val, y_pred_val, y_pred_probs_val, y_val = get_predictions(model,val_data_loader)
+    y_review_texts_train, y_pred_train, y_pred_probs_train, y_train = get_predictions(model,train_data_loader)
+
+    print('Test Data Classification Report : ')
+    print()
+    get_classification_report(y_test,y_pred_test)
+    get_scores(y_test,y_pred_test)
+    get_confusion_matrix(y_test,y_pred_test,class_names)
+    print('Val Data Classification Report : ')
+    print()
+    get_classification_report(y_val,y_pred_val)
+    get_scores(y_val,y_pred_val)
+    get_confusion_matrix(y_val,y_pred_val,class_names)
+    print('\033[96m' + 'Train Data Classification Report : '+ '\033[0m')
+    print()
+    get_classification_report(y_train,y_pred_train)
+    get_scores(y_train,y_pred_train)
+    get_confusion_matrix(y_train,y_pred_train,class_names)
+    
+
+
---
+++ telugu/src/lora_fine_tune.py
@@ -0,0 +1,130 @@
+import torch
+from transformers import AutoModel, AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer, DataCollatorWithPadding
+from peft import LoraConfig, get_peft_model
+from peft import AutoPeftModelForSequenceClassification
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+from marathi.src.evaluate_model import get_confusion_matrix,get_classification_report,get_peft_predictions,get_scores
+from marathi.src.augment_data import random_undersample
+import pandas as pd
+from marathi.src.dataloaders import get_trainer
+
+peft_model_name = 'telugu-bert-lt-edi'
+modified_base = 'telugu-bert-modified-lt-edi'
+base_model = 'l3cube-pune/telugu-bert'
+
+train_df = pd.read_csv('/content/lt-edi-2024/telugu/data/homo_Telugu_train.csv')
+val_df = pd.read_csv('/content/lt-edi-2024/telugu/data/homo_Telugu_dev.csv')
+test_df = pd.read_csv('/content/lt-edi-2024/telugu/data/homo_Telugu_test.csv')
+
+train_df.rename(columns={'Text': 'content','Category ':'label'}, inplace=True)
+val_df.rename(columns={'Text': 'content','Category ':'label'}, inplace=True)
+test_df.rename(columns={'Text': 'content','Category ':'label'}, inplace=True)
+
+# train_df = random_undersample(train_df)
+
+train_df.to_csv('/content/lt-edi-2024/telugu/data/homo_Telugu_train.csv',index = False)
+val_df.to_csv('/content/lt-edi-2024/telugu/data/homo_Telugu_dev.csv',index = False)
+test_df.to_csv('/content/lt-edi-2024/telugu/data/homo_Telugu_test.csv',index = False)
+print('\033[96m' + 'Preprocessed CSV  ready'+ '\033[0m')
+
+train_dataset = load_dataset("csv", data_files='/content/lt-edi-2024/telugu/data/homo_Telugu_train.csv')
+val_dataset = load_dataset("csv", data_files='/content/lt-edi-2024/telugu/data/homo_Telugu_dev.csv')
+test_dataset = load_dataset("csv", data_files='/content/lt-edi-2024/telugu/data/homo_Telugu_test.csv')
+tokenizer = AutoTokenizer.from_pretrained(base_model)
+
+num_labels = 3
+class_names = ['None of the categories','Homophobia','Transphobia']
+
+id2label = {i: label for i, label in enumerate(class_names)}
+label2id = {label: i for i,label in enumerate(class_names)}
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer, return_tensors="pt")
+
+def preprocess(examples):
+    examples["label"] = [label2id[label] for label in examples["label"]]
+    tokenized = tokenizer(examples['content'], truncation=True, padding=True)
+    return tokenized
+
+# def preprocess_test(examples):
+#     # examples["label"] = [label2id[label] for label in examples["label"]]
+#     tokenized = tokenizer(examples['content'], truncation=True, padding=True)
+#     return tokenized
+
+tokenized_dataset_train = train_dataset.map(preprocess, batched=True,  remove_columns=["content"])
+tokenized_dataset_val = val_dataset.map(preprocess, batched=True,  remove_columns=["content"])
+tokenized_dataset_test = test_dataset.map(preprocess, batched=True,  remove_columns=["content"])
+
+train_dataset=tokenized_dataset_train['train']
+val_dataset=tokenized_dataset_val['train']
+test_dataset = tokenized_dataset_test['train']
+print('\033[96m' + 'Datasets ready'+ '\033[0m')
+print()
+
+training_args = TrainingArguments(
+    output_dir='./results',
+    evaluation_strategy='epoch',
+    learning_rate=2e-5,
+    num_train_epochs=3,
+    per_device_train_batch_size=8,
+)
+print('\033[96m' + 'Training arguments set.'+ '\033[0m')
+print()
+
+
+model = AutoModelForSequenceClassification.from_pretrained(base_model, id2label=id2label)
+print('\033[96m' + 'Loaded pretrained Telugu BERT'+ '\033[0m')
+print()
+
+peft_config = LoraConfig(task_type="SEQ_CLS", inference_mode=False, r=8, lora_alpha=16, lora_dropout=0.1,target_modules='all-linear')
+peft_model = get_peft_model(model, peft_config)
+
+print('PEFT Model')
+peft_model.print_trainable_parameters()
+
+peft_lora_finetuning_trainer = get_trainer(peft_model)
+print('\033[96m' + 'Training the peft model...'+ '\033[0m')
+print()
+peft_lora_finetuning_trainer.train()
+peft_lora_finetuning_trainer.evaluate()
+print('\033[96m' + 'Saving fine tuned peft model...'+ '\033[0m')
+print()
+tokenizer.save_pretrained(modified_base)
+peft_model.save_pretrained(peft_model_name)
+print('\033[96m' + 'Saved the peft model'+ '\033[0m')
+print()
+inference_model = AutoPeftModelForSequenceClassification.from_pretrained(peft_model_name, id2label=id2label).to('cuda')
+tokenizer = AutoTokenizer.from_pretrained(modified_base)
+print('\033[96m' + 'Loaded Trained Model for inference'+ '\033[0m')
+print()
+
+
+train_data_loader = DataLoader(train_dataset, batch_size=16, collate_fn=data_collator)
+val_data_loader = DataLoader(val_dataset, batch_size=16, collate_fn=data_collator)
+test_data_loader = DataLoader(test_dataset, batch_size=16, collate_fn=data_collator)
+
+print('\033[96m' + 'Getting Predictions...'+ '\033[0m')
+print()
+y_pred_test, y_pred_probs_test, y_test = get_peft_predictions(model,test_data_loader)
+y_pred_val, y_pred_probs_val, y_val = get_peft_predictions(model,test_data_loader)
+y_pred_train, y_pred_probs_train, y_train = get_peft_predictions(inference_model,train_data_loader)
+
+print('\033[96m' + 'Test Data Classification Report : '+ '\033[0m')
+print()
+get_classification_report(y_test,y_pred_test)
+get_scores(y_test,y_pred_test)
+get_confusion_matrix(y_test,y_pred_test,class_names)
+print()
+
+print('\033[96m' + 'Val Data Classification Report : '+ '\033[0m')
+print()
+get_classification_report(y_val,y_pred_val)
+get_scores(y_val,y_pred_val)
+get_confusion_matrix(y_val,y_pred_val,class_names)
+print()
+
+print('\033[96m' + 'Train Data Classification Report : '+ '\033[0m')
+print()
+get_classification_report(y_train,y_pred_train)
+get_scores(y_train,y_pred_train)
+get_confusion_matrix(y_train,y_pred_train,class_names)
+# generate_submission_lora_track_3(inference_model,test_data_loader,label2id)
